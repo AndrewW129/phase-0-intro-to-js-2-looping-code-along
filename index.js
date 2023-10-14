d3a9bc4efad174742dd0272b2a1214ef14e90e9d
Andrew Wilson <adwilson12901@gmail.com>
2023-10-14 13:43:22 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const cardName = [];
+
+function writeCards(cardName) {
+    let thankYou = [];
+    let c = 0;
+    while (c < cardName.length) {
+        let message = (`Thank you, ${cardName[c]}, for the wonderful surprise gift!`);
+        thankYou.push(message);
+        c++;
+    }
+
+    return thankYou
+}
+let messages = writeCards(cardName);
+console.log(messages);
+
+let numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+function countDown(numbers) {
+    let i = numbers;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(numbers);
